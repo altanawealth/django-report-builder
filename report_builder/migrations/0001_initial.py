94fd6bdcb20dde86d7a325bace9778f5bfe9d3ff
Altana Wealth <admin@altanawealth.com>
2015-09-16 11:25:01 +0200

    Update 0001_initial.py
    Add account dependencies so migrate can run

--- report_builder/migrations/0001_initial.py
+++ report_builder/migrations/0001_initial.py
@@ -10,6 +10,7 @@ class Migration(migrations.Migration):
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('contenttypes', '0001_initial'),
+        ('account', '0003_auto_20150107_1224'),
     ]
 
     operations = [
